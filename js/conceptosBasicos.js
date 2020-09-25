fffcb88dacafc139d2c6ce9fc671cbc4e9416c2c
Diego Restrepo <alejorvela@gmail.com>
2020-09-24 22:51:54 -0500

    primeros ejercicios de aritmetica con bootstrap, template string y manejo del doom basico

---
+++ js/conceptosBasicos.js
@@ -0,0 +1,18 @@
+let n1 = 1;
+var n2 = 2;
+let sum = n1 + n2;
+let div = n1 / n2;
+let res = n1 - n2;
+let produc = n1 * n2;
+
+document.getElementById("solucion").innerHTML = 
+`<p class="text-danger">Resultado de la suma: ${sum}</p>`;
+
+document.getElementById("solucion").innerHTML += 
+`<p class="text-primary">Resultado de la división: ${div}</p>`;
+
+document.getElementById("solucion").innerHTML += 
+`<p class="text-warning">Resultado de la resta ${res}</p>`;
+
+document.getElementById("solucion").innetHTML += 
+`<p class="text-info">Resultado del producto: ${produc}</p>`;
